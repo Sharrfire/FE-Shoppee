4c8833a7fe74a02cec56467ead12ecb68dca2356
DinhNgocThai <56432733+Sharrfire@users.noreply.github.com>
2022-07-16 16:16:56 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import CartFeature from './features/Cart';
+import ProductFeature from './features/Product';
+import DetailPage from './features/Product/pages/DetailPage';
+import ListPage from './features/Product/pages/ListPage';
+import NotFound from './pages/NotFound';
+function App() {
+  return (
+    <div className='app'>
+      <Header />
+      <Routes>
+        <Route path='/home' element={<Navigate replace to='/products' />} />
+        <Route path='/' element={<Navigate replace to='/products' />} />
+        <Route path='products/*' element={<ProductFeature />}>
+          <Route path='' element={<ListPage />} />
+          <Route path=':productId/*' element={<DetailPage />}></Route>
+        </Route>
+        <Route path='/cart' element={<CartFeature />} />
+
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default App;
